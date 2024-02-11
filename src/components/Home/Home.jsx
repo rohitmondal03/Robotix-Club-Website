9a7b52a7d0fdee854a25d09ecf42bfd5d3a981c3
AmanDhananjay <112410719+AmanDhananjay@users.noreply.github.com>
2024-02-11 11:03:47 +0530

    conflit solve

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 
 import React, { memo, useState, useEffect } from 'react';
 
-import React, { memo } from 'react'
+
 import  {  useRef } from "react";
 import "./Home.css"
 import { gsap } from "gsap";
